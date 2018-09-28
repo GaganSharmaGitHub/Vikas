403196439253e64db027e5e7aa9260018c23cf3a
Gagan Sharma <43646043+codeongags@users.noreply.github.com>
2018-09-28 19:39:18 +0530

    Initial commit

---
+++ app/src/main/java/cbpgec1/com/protype/MainActivity.java
@@ -0,0 +1,13 @@
+package cbpgec1.com.protype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
